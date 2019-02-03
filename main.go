1053eb505ee610719f9948aa81cd9d5bed160385
Dima Afanasyev <dimaafanasev98@gmail.com>
2019-02-03 08:51:57 +0300

    initial structure based on chi router example

--- main.go
+++ main.go
@@ -1,26 +1,119 @@
+//
+// REST
+// ====
+// This example demonstrates a HTTP REST web service with some fixture data.
+// Follow along the example and patterns.
+//
+// Also check routes.json for the generated docs from passing the -routes flag
+//
+// Boot the server:
+// ----------------
+// $ go run main.go
+//
+// Client requests:
+// ----------------
+// $ curl http://localhost:3333/
+// root.
+//
+// $ curl http://localhost:3333/articles
+// [{"id":"1","title":"Hi"},{"id":"2","title":"sup"}]
+//
+// $ curl http://localhost:3333/articles/1
+// {"id":"1","title":"Hi"}
+//
+// $ curl -X DELETE http://localhost:3333/articles/1
+// {"id":"1","title":"Hi"}
+//
+// $ curl http://localhost:3333/articles/1
+// "Not Found"
+//
+// $ curl -X POST -d '{"id":"will-be-omitted","title":"awesomeness"}' http://localhost:3333/articles
+// {"id":"97","title":"awesomeness"}
+//
+// $ curl http://localhost:3333/articles/97
+// {"id":"97","title":"awesomeness"}
+//
+// $ curl http://localhost:3333/articles
+// [{"id":"2","title":"sup"},{"id":"97","title":"awesomeness"}]
+//
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
+	"net/http"
 
-	"github.com/buaazp/fasthttprouter"
-	"github.com/valyala/fasthttp"
+	"github.com/go-chi/docgen"
+	"github.com/go-chi/render"
 )
 
-func index(c *fasthttp.RequestCtx) {
-	fmt.Fprint(c, "Welcome!\n")
-}
-
-func hello(c *fasthttp.RequestCtx) {
-	fmt.Fprintf(c, "Hello, %s\n", ctx.UserValue("name"))
-}
+var routes = flag.Bool("routes", false, "Generate router documentation")
 
 func main() {
-	router := fasthttprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-	router.GET("/static/*file", fasthttp.FSHandler("./static", 0))
+	flag.Parse()
+	r := NewRouter()
 
-	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+	// Passing -routes to the program will generate docs for the above
+	// router definition. See the `routes.json` file in this folder for
+	// the output.
+	if *routes {
+		// fmt.Println(docgen.JSONRoutesDoc(r))
+		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
+			ProjectPath: "github.com/go-chi/chi",
+			Intro:       "Welcome to the chi/_examples/rest generated docs.",
+		}))
+		return
+	}
+	http.ListenAndServe(":3333", r)
 }
+
+
+
+// NOTE: as a thought, the request and response payloads for an Article could be the
+// same payload type, perhaps will do an example with it as well.
+// type ArticlePayload struct {
+//   *Article
+// }
+
+//--
+// Error response payloads & renderers
+//--
+
+// ErrResponse renderer type for handling all sorts of errors.
+//
+// In the best case scenario, the excellent github.com/pkg/errors package
+// helps reveal information on the error, setting it on Err, and in the Render()
+// method, using it to set the application-specific error code in AppCode.
+type ErrResponse struct {
+	Err            error `json:"-"` // low-level runtime error
+	HTTPStatusCode int   `json:"-"` // http response status code
+
+	StatusText string `json:"status"`          // user-level status message
+	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func ErrInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
+}
+
+func ErrRender(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 422,
+		StatusText:     "Error rendering response.",
+		ErrorText:      err.Error(),
+	}
+}
+
+var ErrNotFound = &ErrResponse{HTTPStatusCode: 404, StatusText: "Resource not found."}
