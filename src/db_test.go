61ecbb1bd40e3487f517bf83c4eb517aa9febf62
Dima Afanasyev <dimaafanasev98@gmail.com>
2019-02-08 10:11:21 +0300

    refactored tests

--- src/db_test.go
+++ src/db_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"strings"
 
@@ -21,32 +22,11 @@ func TestEncodeDecode(t *testing.T) {
 }
 
 func TestSingleUrl(t *testing.T) {
-	conn := redigomock.NewConn()
-	conn.Clear()
-	// data
-	url := "https://www.google.com/search?q=golang+spec"
-	parsed, err := ParseURL(url)
-	if err != nil {
-		t.Errorf("Error during parsing valid url: %s\n", err)
-	}
-	// saving & shortening
-	id := int64(9910050000)
-	conn.Command("GET", IdKey).Expect(id)
-	conn.Command("INCR", IdKey).Expect(id + 1)
-	conn.Command("SET", id, url).Expect("ok")
-
-	shorten, err := parsed.SaveURL(conn)
-	if err != nil {
-		t.Errorf("error shortening url: %s\n", err)
-	}
-	// fetching
-	conn.Clear()
-	conn.Command("GET", id).Expect(url)
-	decoded, err := DbGetURL(shorten, conn)
+	url := "https://www.google.com/search?q=golang+specification"
+	_, decoded, err := process(url, int64(1))
 	if err != nil {
 		t.Errorf("error fetching url: %s\n", err)
-	}
-	if strings.Compare(decoded, url) != 0 {
+	} else if strings.Compare(decoded, url) != 0 {
 		t.Errorf("urls does not match: %s and %s\n", url, decoded)
 	}
 }
@@ -56,9 +36,39 @@ func TestMultipleUrls(t *testing.T) {
 }
 
 func TestDuplicateUrl(t *testing.T) {
-
+	url := "https://reddit.com"
+	short1, _, err := process(url, int64(1))
+	short2, _, err := process(url, int64(2))
+	fmt.Printf("short1: %v, short2: %v", short1, short2)
+	if strings.Compare(short1, short2) != 0 || err != nil {
+		t.Errorf("same url processed twice: %v", url)
+	}
 }
 
 func TestInvalidUrl(t *testing.T) {
 
+}
+
+func process(data string, id int64) (string, string, error) {
+	conn := redigomock.NewConn()
+	conn.Clear()
+	// data
+	url, err := ParseURL(data)
+	if err != nil {
+		return "", "", err
+	}
+	// saving & processing
+	conn.Command("GET", IdKey).Expect(id)
+	conn.Command("INCR", IdKey).Expect(id + 1)
+	conn.Command("SET", id, data).Expect("ok")
+
+	shorten, err := url.SaveURL(conn)
+	if err != nil {
+		return "", "", err
+	}
+	// fetching
+	conn.Clear()
+	conn.Command("GET", id).Expect(data)
+	dbURL, err := DbGetURL(shorten, conn)
+	return shorten, dbURL, err
 }
